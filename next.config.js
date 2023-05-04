196cb40cffd5913ae50c998ad5e3b13b50015b5b
Joanna.chen <joanna.chen@ewinsonic.com>
2023-05-04 21:24:58 +0800

    build(next.config.js, service): upadte baseURL by env config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   transpilePackages: ["antd"],
+  env: {
+    rootUrl: process.env.NODE_ENV === 'production' 
+      ? 'https://hookloop-server.onrender.com/' 
+      : 'http://localhost:8080',
+  },
 };
 
 module.exports = nextConfig;
