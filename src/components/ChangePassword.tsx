bde029010b95ec44c6e72cf6f5cc494375036d65
jason <yulaie1012@gmail.com>
2023-05-07 14:56:56 +0800

    feat: add <ChangePassword /> component

---
+++ src/components/ChangePassword.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Modal, Form, Input, Button, message as msg } from "antd";
+import { updatePassword } from "@/service/api";
+import { IApiResponse } from "@/service/instance";
+
+interface IChangePassword {
+  open: boolean;
+  close: () => void;
+}
+
+const ChangePassword: React.FC<IChangePassword> = (props) => {
+  const { open, close } = props;
+  const [form] = Form.useForm();
+
+  const handleCancel = (): void => {
+    // ...
+    close();
+  };
+
+  const onFinish = async (values: { oldPassword: string; newPassword: string; confirmPassword: string }) => {
+    // 驗證密碼和確認密碼是否相同
+    if (values.newPassword !== values.confirmPassword) {
+      msg.error("密碼不一致");
+      return;
+    }
+
+    // 處理修改密碼邏輯
+    try {
+      const { oldPassword, newPassword } = values;
+      const res = await updatePassword({ oldPassword, newPassword });
+      const { message, status } = res as unknown as IApiResponse;
+
+      if (status === "success") {
+        msg.success(message);
+        close();
+      } else {
+        msg.error(message);
+      }
+    } catch (error) {
+      msg.error("密碼修改失敗");
+      console.error(error);
+    }
+  };
+
+  return (
+    <Modal width={685} title="Change Password" destroyOnClose open={open} onCancel={handleCancel} footer={null}>
+      <div className="flex flex-col items-center p-[25px] pt-[7px]">
+        <Form
+          name="changePassword"
+          form={form}
+          labelCol={{ span: 6 }}
+          labelAlign="left"
+          onFinish={onFinish}
+          className="w-full flex flex-col items-center gap-[20px]"
+        >
+          <Form.Item
+            name="oldPassword"
+            className="w-full"
+            label="Old Password"
+            rules={[{ required: true, message: "Please enter your old password" }]}
+          >
+            <Input.Password minLength={8} maxLength={20} />
+          </Form.Item>
+
+          <Form.Item
+            name="newPassword"
+            className="w-full"
+            label="New Password"
+            rules={[{ required: true, message: "Please enter your new password" }]}
+          >
+            <Input.Password minLength={8} maxLength={20} />
+          </Form.Item>
+
+          <Form.Item
+            name="confirmPassword"
+            className="w-full"
+            label="Confirm Password"
+            rules={[{ required: true, message: "Please confirm your new password" }]}
+          >
+            <Input.Password minLength={8} maxLength={20} />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="w-[120px] h-[32px] py-[4px] px-[15px] font-semibold">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </Modal>
+  );
+};
+
+export default ChangePassword;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import ChangePassword from "@/components/ChangePassword";
+import { Button } from "antd";
+import React, { useState } from "react";
 
 const Profile = () => {
+  const [s_showChangePassword, set_s_showChangePassword] = useState(false);
+
+  const closeChangePassword = (): void => {
+    set_s_showChangePassword(false);
+  };
+
   return (
     <section className="h-full flex-center">
       <h1 className="text-[100px]">Profile</h1>
+      <Button className="font-bold text-black" onClick={() => set_s_showChangePassword(true)}>
+        Change Password
+      </Button>
+      <ChangePassword open={s_showChangePassword} close={closeChangePassword} />
     </section>
   );
 };
