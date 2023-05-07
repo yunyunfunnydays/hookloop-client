18e23bce43e6069f578ed9806f65e3dcaeec2c76
jason <yulaie1012@gmail.com>
2023-05-07 21:09:39 +0800

    feat: add <profile /> component page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,8 @@
 import ChangePassword from "@/components/ChangePassword";
-import { Button } from "antd";
+import { Button, Form, Row, Col, Typography, Input, Divider } from "antd";
 import React, { useState } from "react";
+
+const { Title } = Typography;
 
 const Profile = () => {
   const [s_showChangePassword, set_s_showChangePassword] = useState(false);
@@ -9,12 +11,85 @@ const Profile = () => {
     set_s_showChangePassword(false);
   };
 
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+
   return (
-    <section className="h-full flex-center">
-      <h1 className="text-[100px]">Profile</h1>
-      <Button className="font-bold text-black" onClick={() => set_s_showChangePassword(true)}>
-        Change Password
-      </Button>
+    <section className="h-full flex justify-center">
+      <div className="w-[685px] mt-8">
+        <section className="mb-8">
+          <Title level={2}>Basic Info</Title>
+          <Divider />
+          <Form
+            labelCol={{ span: 6 }}
+            labelAlign="left"
+            onFinish={onFinish}
+            className="w-full flex flex-col items-center gap-[20px] mb-4"
+          >
+            <Form.Item name="username" label="Username" className="w-full">
+              <Input />
+            </Form.Item>
+            <Row className="w-full">
+              <Col span={6} className="leading-[32px]">
+                Password
+              </Col>
+              <Col span={18}>
+                <Button type="text" onClick={() => set_s_showChangePassword(true)} className="font-bold p-0">
+                  Change Password
+                </Button>
+              </Col>
+            </Row>
+            <Form.Item name="email" label="Email" className="w-full">
+              <Input disabled />
+            </Form.Item>
+          </Form>
+        </section>
+        <section className="mb-8">
+          <Title level={2}>Preference</Title>
+          <Divider />
+          <Form
+            labelCol={{ span: 6 }}
+            labelAlign="left"
+            onFinish={onFinish}
+            className="w-full flex flex-col items-center gap-[20px] mb-4"
+          >
+            <Form.Item name="language" label="Language" className="w-full">
+              <Input disabled defaultValue="English (United States) [Default]" />
+            </Form.Item>
+            <Form.Item name="colorTheme" label="Color Theme" className="w-full">
+              <Input disabled defaultValue="Normal" />
+            </Form.Item>
+          </Form>
+        </section>
+        <section>
+          <Title level={2}>Groups</Title>
+          <Divider />
+          <Row className="w-full mb-[20px]">
+            <Col span={6} className="leading-[32px]">
+              Workspaces
+            </Col>
+            <Col span={18}>
+              <Button type="text" onClick={() => set_s_showChangePassword(true)} className="font-bold p-0">
+                Workspace1
+              </Button>
+            </Col>
+          </Row>
+          <Row className="w-full">
+            <Col span={6} className="leading-[32px]">
+              Members
+            </Col>
+            <Col span={18}>
+              <Button type="text" onClick={() => set_s_showChangePassword(true)} className="font-bold p-0">
+                Member1
+              </Button>
+            </Col>
+          </Row>
+        </section>
+        <Button danger className="mt-6 ">
+          Close Account
+        </Button>
+      </div>
       <ChangePassword open={s_showChangePassword} close={closeChangePassword} />
     </section>
   );
