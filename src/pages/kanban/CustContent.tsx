f1e103423d2e942e6b09513cfca6c2fb49dd2067
jason <yulaie1012@gmail.com>
2023-05-17 01:25:56 +0800

    chore: remove unnecessary eslint rule

--- src/pages/kanban/CustContent.tsx
+++ src/pages/kanban/CustContent.tsx
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-
 import React, { useState } from "react";
 import {
   BellFilled,
