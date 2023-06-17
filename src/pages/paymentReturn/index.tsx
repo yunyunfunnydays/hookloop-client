64b8b13d2f8bf3c14ea445d17cc13b9e6094fb47
Joanna.chen <joanna.chen@ewinsonic.com>
2023-06-18 02:13:16 +0800

    fix: fix router push in paymentReturn

--- src/pages/paymentReturn/index.tsx
+++ src/pages/paymentReturn/index.tsx
@@ -1,4 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const PaymentReturn = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(window.location.href);
+  }, []);
+
   return <>付款結束，藍新金流導轉前端畫面</>;
 };
 export default PaymentReturn;
