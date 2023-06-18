054c3e2e89e34a1aae9592f30484978d1db4cbed
yunyunfunnydays <yunhsuan2017@gmail.com>
2023-06-18 20:39:07 +0800

    fix: add Authorization headers

--- src/service/apis/card.ts
+++ src/service/apis/card.ts
@@ -4,7 +4,11 @@ import instance from "../instance";
 
 // 新建 kanban
 export const addCard = (data: Pick<ICard, "name" | "kanbanId" | "listId">) => {
-  return instance.post("cards", data);
+  return instance.post("cards", data, {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
+    },
+  });
 };
 
 // 使用 cardId 取得卡片
@@ -18,13 +22,18 @@ export const getCardById = (cardId: string) => {
 
 // 使用 cardId 更新卡片
 export const updateCard = (kanbanId: string, cardId: string, data: ICard) => {
-  return instance.patch(`cards/${kanbanId}/${cardId}/update`, data);
+  return instance.patch(`cards/${kanbanId}/${cardId}/update`, data, {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
+    },
+  });
 };
 
 // 上傳檔案
 export const addAttachment = (cardId: string, data: any) => {
   return instance.post(`cards/${cardId}/attachment`, data, {
     headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
       "Content-Type": "multipart/form-data",
     },
   });
@@ -32,7 +41,12 @@ export const addAttachment = (cardId: string, data: any) => {
 
 // 刪除檔案
 export const deleteAttachment = (cardId: string, attachmentId: string) => {
-  return instance.delete(`cards/${cardId}/attachment/${attachmentId}`);
+  return instance.delete(`cards/${cardId}/attachment/${attachmentId}`, {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
+      "Content-Type": "multipart/form-data",
+    },
+  });
 };
 
 // 移動卡片
@@ -44,15 +58,27 @@ export const moveCard = (data: {
   oldCardOrder: string[];
   socketData: any;
 }) => {
-  return instance.patch("cards/move", data);
+  return instance.patch("cards/move", data, {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
+    },
+  });
 };
 
 // 取得 card 上所有 commits
 export const getComments = (cardId: string) => {
-  return instance.get(`cards/${cardId}/comment`);
+  return instance.get(`cards/${cardId}/comment`, {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
+    },
+  });
 };
 
 // 新增 card 留言
 export const addComment = (cardId: string, data: { currentComment: string; userId: string }) => {
-  return instance.post(`cards/${cardId}/comment`, data);
+  return instance.post(`cards/${cardId}/comment`, data, {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
+    },
+  });
 };
--- src/service/apis/kanban.ts
+++ src/service/apis/kanban.ts
@@ -4,8 +4,11 @@ import instance from "../instance";
 
 // 取得看板資料
 export const getKanbanByKey = (kanbanId: string, query?: any, userId?: string) => {
-  // return instance.get(`kanbans/${kanbanId}`, { params: query }); 新增一個 userId 參數
-  return instance.get(`kanbans/all/${kanbanId}`, { params: { query, userId } });
+  return instance.get(`kanbans/${kanbanId}`, {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
+    },
+  });
 };
 
 // 新建 kanban
@@ -39,12 +42,22 @@ export const getTags = (kanbanId: string) => {
 
 // 新增 Tag
 export const createTag = (kanbanId: string, data: Pick<ITag, "name" | "color" | "icon">) => {
-  return instance.post(`kanbans/${kanbanId}/tag`, data);
+  return instance.post(`kanbans/${kanbanId}/tag`, data, {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
+      "Content-Type": "multipart/form-data",
+    },
+  });
 };
 
 // 更新 Tag
 export const updateTag = (kanbanId: string, data: Pick<ITag, "name" | "color" | "icon">, tagId: string = "") => {
-  return instance.patch(`kanbans/${kanbanId}/tag/${tagId}`, data);
+  return instance.patch(`kanbans/${kanbanId}/tag/${tagId}`, data, {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("hookloop-token")}`,
+      "Content-Type": "multipart/form-data",
+    },
+  });
 };
 
 // 取得看板下所有 lists
